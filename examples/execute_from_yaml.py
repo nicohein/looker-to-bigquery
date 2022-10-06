50e8a1c732c8f85de1fcf117f112adad50b18641
rahultesla <rascimat@gmail.com>
2022-10-07 01:02:38 +0200

    Revert "added files"
    This reverts commit f4f2746609d0c698d5ff92d0af7c874c0177a201.

--- examples/execute_from_yaml.py
+++
@@ -1,5 +0,0 @@
-from look2bq.executors import YamlExecuter
-
-yaml_path = "events_config.yaml"
-exe = YamlExecuter(yaml_path)
-exe.execute()
--- examples/looker_to_bigquery_upload.py
+++
@@ -1,4 +0,0 @@
-from look2bq.jobs import Look2Bq
-
-job = Look2Bq()
-job.run(qid="QueryIdCodeCopiedFromUrl", destination="project.dataset.tablename", if_exists="replace")
